6fefe37b1029f7ff38e906a1ee463ad3ea41ad23
Chad Rhiger <csrhig@gmail.com>
2022-03-27 15:42:24 -0500

    reviewed assignments and fixed shoeCount to reflect actual instructions (i am learninggit add .). retested code to make sure it was functional and complete.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -43,8 +43,8 @@ console.log(me.favThreeFoods[me.favThreeFoods.length -1]);
 */
 
 console.log(`my shoeCount should be 6. Actual shoeCount: ${me.shoeCount}`);
-me.shoeCount += 2;
-console.log('shoeCount should now be 8. Actual shoeCount:', me.shoeCount);
+me.shoeCount += 1;
+console.log('shoeCount should now be 7. Actual shoeCount:', me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -55,3 +55,7 @@ console.log('shoeCount should now be 8. Actual shoeCount:', me.shoeCount);
 
 me.favoriteColor = 'blue'
 console.log(`My favorite color should be blue. It is: ${me.favoriteColor}`);
+
+me.favoriteCar = '1948 Tucker Torpedo'
+console.log(`My favorite car (totally not because I saw the biopic of the same name when I was 11) should be a 1948 Tucker Torpedo. It is a: ${me.favoriteCar}`);
+
